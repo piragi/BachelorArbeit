3173ab2f572b400b77ef4308a6cab4856ded9089
Julius Sula <julius.sula@gmail.com>
2022-05-16 17:30:06 +0200

    level1 animation

--- app/src/main/java/com/example/breathingmeditationandroid/GameScreen.kt
+++ app/src/main/java/com/example/breathingmeditationandroid/GameScreen.kt
@@ -1,6 +1,5 @@
 package com.example.breathingmeditationandroid
 
-import android.animation.ValueAnimator
 import android.bluetooth.BluetoothDevice
 import android.content.ComponentName
 import android.content.Context
@@ -10,13 +9,15 @@ import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.view.View.INVISIBLE
+import android.view.animation.*
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
+import com.plattysoft.leonids.ParticleSystem
 import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
+
 
 class GameScreen : ComponentActivity() {
 
@@ -38,7 +39,7 @@ class GameScreen : ComponentActivity() {
 
 
             breathingUtils = BreathingUtils(mService)
-            breathingUtils.calibrateBreathing()
+            //breathingUtils.calibrateBreathing()
             startLevel()
 
         }
@@ -73,16 +74,80 @@ class GameScreen : ComponentActivity() {
     fun startLevel() {
         thread(start = true, isDaemon = true) {
             lifecycleScope.launch {
-                breathingUtils.deepBreathDetected()
+                //breathingUtils.deepBreathDetected()
                 deepBreathAnimation()
             }
         }
     }
 
     private fun deepBreathAnimation() {
+        val snowParticleSystem = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
+            .setScaleRange(0.1f, 0.2f)
+            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
+            .setRotationSpeedRange(90f, 180f)
+            .setAcceleration(0.00013f, 90)
+            .setFadeOut(200, AccelerateInterpolator())
+
+        val snowParticleSystem2 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
+            .setScaleRange(0.1f, 0.2f)
+            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
+            .setRotationSpeedRange(90f, 180f)
+            .setAcceleration(0.00013f, 90)
+            .setFadeOut(200, AccelerateInterpolator())
+
+        val snowParticleSystem3 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
+            .setScaleRange(0.1f, 0.2f)
+            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
+            .setRotationSpeedRange(90f, 180f)
+            .setAcceleration(0.00013f, 90)
+            .setFadeOut(200, AccelerateInterpolator())
+
+        val snowParticleSystem4 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
+            .setScaleRange(0.1f, 0.2f)
+            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
+            .setRotationSpeedRange(90f, 180f)
+            .setAcceleration(0.00013f, 90)
+            .setFadeOut(200, AccelerateInterpolator())
+
+        val snowParticleSystem5 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
+            .setScaleRange(0.1f, 0.2f)
+            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
+            .setRotationSpeedRange(90f, 180f)
+            .setAcceleration(0.00013f, 90)
+            .setFadeOut(200, AccelerateInterpolator())
+
         val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout)
-        Log.i("we got here", "nice")
         snow.startAnimation(fadeOut)
+
+        fadeOut.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                Log.i("info", "onStartOben")
+                snowParticleSystem.emit(50,200, 5)
+                snowParticleSystem2.emit(600, 200, 5)
+                snowParticleSystem3.emit(1200, 200, 5)
+                snowParticleSystem4.emit(1500, 380, 5)
+                snowParticleSystem5.emit(1750, 150, 5)
+                Log.i("info", "onStartUnten")
+
+            }
+            override fun onAnimationEnd(animation: Animation) {
+                snow.visibility = INVISIBLE
+                Log.i("info", "onEndOben")
+                snowParticleSystem.stopEmitting()
+                snowParticleSystem2.stopEmitting()
+                snowParticleSystem3.stopEmitting()
+                snowParticleSystem4.stopEmitting()
+                snowParticleSystem5.stopEmitting()
+                Log.i("info", "onEndUnten")
+            }
+            override fun onAnimationRepeat(p0: Animation?) {}
+        })
+
+        fadeOut.cancel()
+
+
+
+
 
         //on Animation End make it invisible
     }
---
+++ app/src/main/java/com/example/breathingmeditationandroid/ShowOffJava.java
@@ -0,0 +1,23 @@
+package com.example.breathingmeditationandroid;
+
+public class ShowOffJava {
+
+    private int value1;
+    private String value2;
+
+    public ShowOffJava(int value1, String value2) {
+        this.value1 = value1;
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+    public int getValue2() {
+        return value1;
+    }
+    public void setValue2(String value2) {
+    }
+}
