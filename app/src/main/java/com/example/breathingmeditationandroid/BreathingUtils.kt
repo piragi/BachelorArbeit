9456f7dbae0548fca5e86ced49d2e98005a7e03b
Julius Šula <julius.sula@gmail.com>
2022-05-17 14:12:55 +0200

    add deepChest, staccato

--- app/src/main/java/com/example/breathingmeditationandroid/BreathingUtils.kt
+++ app/src/main/java/com/example/breathingmeditationandroid/BreathingUtils.kt
@@ -119,15 +119,50 @@ class BreathingUtils(mService: BluetoothConnection) {
         return Pair(medianAbdo, medianThor)
     }
 
-    fun deepBreathDetected() {
-        while (mService.mAbdoCorrected < calibratedAbdo.second * 0.95
-            || mService.mThorCorrected < calibratedThor.second * 0.95
-        ) {
+    fun deepBellyBreathDetected() {
+        while (mService.mAbdoCorrected < calibratedAbdo.second * 0.95)
+        {
             Thread.sleep(2)
         }
 
         while (mService.mExpiration == 0) {
         }
+    }
+
+    fun deepChestBreathDetected() {
+        while (mService.mThorCorrected < calibratedThor.second * 0.95)
+        {
+            Thread.sleep(2)
+        }
+
+        while (mService.mExpiration == 0) {
+        }
+    }
+
+    fun staccatoBreathDetected() {
+
+        //for every breath > half of max
+        //Expiration == 0
+        //for every changed value
+        //buffer of 3
+        //if difference of 20% between first and last
+        //staccato ->
+
+        val bufferStaccato: ArrayList<Double> = ArrayList()
+
+        if(mService.mExpiration == 0 && mService.mThorCorrected > calibratedThor.second * 0.6) {
+            //everytime there is a updated value -> add to buffer
+            if(bufferStaccato.size == 3) {
+                bufferStaccato.removeAt(0)
+                bufferStaccato.add(mService.mThorCorrected)
+                if (bufferStaccato[2] >= bufferStaccato[0]*1.2) {
+                    Log.i("staccato", "detected")
+                }
+            } else {
+                bufferStaccato.add(mService.mThorCorrected)
+            }
+        }
+
     }
 
     fun detectRespiration(prev: Pair<Double, Double>, curr: Pair<Double, Double>): Boolean {
--- app/src/main/java/com/example/breathingmeditationandroid/GameScreen.kt
+++ app/src/main/java/com/example/breathingmeditationandroid/GameScreen.kt
@@ -40,7 +40,7 @@ class GameScreen : ComponentActivity() {
 
             breathingUtils = BreathingUtils(mService)
             //breathingUtils.calibrateBreathing()
-            startLevel()
+            //startLevel()
 
         }
 
@@ -59,7 +59,7 @@ class GameScreen : ComponentActivity() {
         setContentView(R.layout.game_screen)
         snow = findViewById<View>(R.id.snow) as ImageView
 
-        deepBreathAnimation()
+        deepBellyBreathAnimation()
 
         //setup and start bluetooth service
         mDevice = intent?.extras?.getParcelable("Device")
@@ -74,82 +74,64 @@ class GameScreen : ComponentActivity() {
     fun startLevel() {
         thread(start = true, isDaemon = true) {
             lifecycleScope.launch {
-                //breathingUtils.deepBreathDetected()
-                deepBreathAnimation()
+                //breathingUtils.deepBellyBreathDetected()
+                deepBellyBreathAnimation()
             }
         }
     }
 
-    private fun deepBreathAnimation() {
-        val snowParticleSystem = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
-            .setScaleRange(0.1f, 0.2f)
-            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
-            .setRotationSpeedRange(90f, 180f)
-            .setAcceleration(0.00013f, 90)
-            .setFadeOut(200, AccelerateInterpolator())
-
-        val snowParticleSystem2 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
-            .setScaleRange(0.1f, 0.2f)
-            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
-            .setRotationSpeedRange(90f, 180f)
-            .setAcceleration(0.00013f, 90)
-            .setFadeOut(200, AccelerateInterpolator())
-
-        val snowParticleSystem3 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
-            .setScaleRange(0.1f, 0.2f)
-            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
-            .setRotationSpeedRange(90f, 180f)
-            .setAcceleration(0.00013f, 90)
-            .setFadeOut(200, AccelerateInterpolator())
-
-        val snowParticleSystem4 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
-            .setScaleRange(0.1f, 0.2f)
-            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
-            .setRotationSpeedRange(90f, 180f)
-            .setAcceleration(0.00013f, 90)
-            .setFadeOut(200, AccelerateInterpolator())
-
-        val snowParticleSystem5 = ParticleSystem(this, 30, R.drawable.snowflake, 1000)
-            .setScaleRange(0.1f, 0.2f)
-            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
-            .setRotationSpeedRange(90f, 180f)
-            .setAcceleration(0.00013f, 90)
-            .setFadeOut(200, AccelerateInterpolator())
+    private fun deepBellyBreathAnimation() {
+        val snowParticleSystem = setSnowParticleSystem()
+        val snowParticleSystem2 = setSnowParticleSystem()
+        val snowParticleSystem3 = setSnowParticleSystem()
+        val snowParticleSystem4 = setSnowParticleSystem()
+        val snowParticleSystem5 = setSnowParticleSystem()
+        val snowParticleSystem6 = setSnowParticleSystem()
 
         val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout)
-        snow.startAnimation(fadeOut)
 
         fadeOut.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation) {
                 Log.i("info", "onStartOben")
-                snowParticleSystem.emit(50,200, 5)
-                snowParticleSystem2.emit(600, 200, 5)
-                snowParticleSystem3.emit(1200, 200, 5)
-                snowParticleSystem4.emit(1500, 380, 5)
-                snowParticleSystem5.emit(1750, 150, 5)
+                //snowParticleSystem.emit(50,200, 0)
+                snowParticleSystem6.emit(50,200, 7)
+                snowParticleSystem2.emit(600, 200, 7)
+                snowParticleSystem3.emit(1200, 200, 7)
+                snowParticleSystem4.emit(1500, 380, 7)
+                snowParticleSystem5.emit(1750, 150, 7)
                 Log.i("info", "onStartUnten")
 
             }
             override fun onAnimationEnd(animation: Animation) {
-                snow.visibility = INVISIBLE
+
+                fadeOut.cancel()
                 Log.i("info", "onEndOben")
-                snowParticleSystem.stopEmitting()
+                //snowParticleSystem.stopEmitting()
                 snowParticleSystem2.stopEmitting()
                 snowParticleSystem3.stopEmitting()
                 snowParticleSystem4.stopEmitting()
                 snowParticleSystem5.stopEmitting()
+                snowParticleSystem6.stopEmitting()
+                snow.visibility = INVISIBLE
                 Log.i("info", "onEndUnten")
+
             }
             override fun onAnimationRepeat(p0: Animation?) {}
         })
 
-        fadeOut.cancel()
+        snow.startAnimation(fadeOut)
 
-
-
-
-
+        //fadeOut.cancel()
         //on Animation End make it invisible
+    }
+
+    private fun setSnowParticleSystem() : ParticleSystem {
+        return ParticleSystem(this, 30, R.drawable.snowflake, 200)
+            .setScaleRange(0.1f, 0.2f)
+            .setSpeedModuleAndAngleRange(0.07f, 0.16f, 200, 300)
+            .setRotationSpeedRange(90f, 180f)
+            .setAcceleration(0.00013f, 90)
+            .setFadeOut(150, AccelerateInterpolator())
     }
 
 
