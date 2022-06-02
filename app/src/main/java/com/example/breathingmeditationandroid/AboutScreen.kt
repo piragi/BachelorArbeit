9fbc7d6a6226e2544c379d4c99da42fecc1a9204
juliamayrhauser <s1068277@stud.sbg.ac.at>
2022-06-02 23:04:53 +0200

    started implementation of return function in AboutScreen

--- app/src/main/java/com/example/breathingmeditationandroid/AboutScreen.kt
+++ app/src/main/java/com/example/breathingmeditationandroid/AboutScreen.kt
@@ -6,22 +6,22 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.os.PersistableBundle
-import android.util.Log
+import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
+import com.plattysoft.leonids.ParticleSystem
 
 class AboutScreen : ComponentActivity() {
     private lateinit var serviceIntent: Intent
     private lateinit var mService: BluetoothConnection
+    private lateinit var breathingUtils: BreathingUtils
+    private lateinit var particlesMain: ParticleSystem
+    private lateinit var particlesSupport: ParticleSystem
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             val binder = service as BluetoothConnection.LocalBinder
             mService = binder.getService()
-            Log.i("Calibration", "start")
-            lifecycleScope.launch { Calibrator.calibrate() }
+            breathingUtils = BreathingUtils(mService)
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
@@ -30,12 +30,43 @@ class AboutScreen : ComponentActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //view
         setContentView(R.layout.about_screen)
 
+        //bind service to activity
         serviceIntent = intent?.extras?.getParcelable("Intent")!!
         applicationContext.bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+    }
+
+    private fun initializeParticles() {
+        particlesMain = ParticleSystem(this, 10, R.drawable.leaf2, 1000)
+        particlesMain.setScaleRange(0.7f, 1.3f)
+            .setSpeedRange(0.05f, 0.1f)
+            .setRotationSpeedRange(50f, 120f)
+            .setFadeOut(500, AccelerateInterpolator())
+            .emit(100, 800, 10)
+        particlesSupport = ParticleSystem(this, 2, R.drawable.leaf1, 500)
+        particlesSupport.setScaleRange(0.7f, 1.3f)
+            .setSpeedRange(0.05f, 0.1f)
+            .setRotationSpeedRange(5f, 50f)
+            .setFadeOut(250, AccelerateInterpolator())
+            .emit(100, 800, 10)
+    }
+
+    private fun moveLeaves() {
+        while(mService.mExpiration == 0) {
+
+        }
+    }
+
+    private fun moveLeavesToRight() {
+
+    }
+
+    private fun moveLeavesToLeft() {
+
     }
 
     override fun onDestroy() {
