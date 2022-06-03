7f1a3b30c881eaab92206241ba574de49ecb891a
Julius Šula <julius.sula@gmail.com>
2022-06-03 17:28:23 +0200

    added trees feedback

--- app/src/main/java/com/example/breathingmeditationandroid/FeedbackTrees.kt
+++ app/src/main/java/com/example/breathingmeditationandroid/FeedbackTrees.kt
@@ -1,41 +1,74 @@
 package com.example.breathingmeditationandroid
 
 import android.graphics.drawable.AnimationDrawable
-import android.media.Image
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import kotlin.concurrent.thread
 
-class FeedbackTrees(private val backgroundScreen: ImageView, private val activity: ComponentActivity, val mService: BluetoothConnection) : InspirationExpirationInterface {
 
-    private lateinit var treesRightAnimation: AnimationDrawable
+class FeedbackTrees(
+    private val treesLeft: ImageView,
+    private val treesRight: ImageView,
+    private val treesNeutral: ImageView,
+    private val activity: ComponentActivity,
+    val mService: BluetoothConnection
+) : InspirationExpirationInterface {
 
     init {
         activity.runOnUiThread {
-            backgroundScreen.setBackgroundResource(R.drawable.trees_right)
-
+            treesRight.setBackgroundResource(R.drawable.trees_right)
+            treesLeft.setBackgroundResource(R.drawable.trees_left)
         }
-
-
         mService.addListener(this)
+        checkForAnimations()
 
     }
-    override fun onInspiration() {
-        Log.i("inspiration", "happened")
-        activity.runOnUiThread() {
-            val treesRightAnimation =
-                backgroundScreen.background as AnimationDrawable
-            treesRightAnimation.start()
-        }
 
+    private fun checkForAnimations() {
+        thread(start = true, isDaemon = true) {
+            val treesLeftAnimation =
+                treesLeft.background as AnimationDrawable
+            while(true) {
+                if(!treesLeftAnimation.isRunning) {
+                    treesNeutral.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     override fun onExpiration() {
-        activity.runOnUiThread() {
+        Log.i("inspiration", "happened")
+        activity.runOnUiThread {
+            treesRight.visibility = View.VISIBLE
+            treesLeft.visibility = View.INVISIBLE
+            treesNeutral.visibility = View.INVISIBLE
+
+            val treesLeftAnimation =
+                treesLeft.background as AnimationDrawable
+            treesLeftAnimation.stop()
+
             val treesRightAnimation =
-                backgroundScreen.background as AnimationDrawable
+                treesRight.background as AnimationDrawable
+            treesRightAnimation.start()
+
+        }
+    }
+
+    override fun onInspiration() {
+        activity.runOnUiThread {
+            treesRight.visibility = View.INVISIBLE
+            treesLeft.visibility = View.VISIBLE
+            treesNeutral.visibility = View.INVISIBLE
+
+
+            val treesRightAnimation =
+                treesRight.background as AnimationDrawable
             treesRightAnimation.stop()
+            val treesLeftAnimation =
+                treesLeft.background as AnimationDrawable
+            treesLeftAnimation.start()
 
         }
     }
